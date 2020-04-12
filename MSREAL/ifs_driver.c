42da0492ba741757af5ba14c379b0124ea7b8ea5
MilanSimin <simin.milan34@gmail.com>
2020-04-12 20:48:36 +0200

    upgrade

--- MSREAL/ifs_driver.c
+++ MSREAL/ifs_driver.c
@@ -21,10 +21,32 @@
 #include <linux/interrupt.h>
 
 
-#define MAX_PKT_LEN 640*480*4
-#define DRIVER_NAME "ifs"
+MODULE_AUTHOR ("FTN");
+MODULE_DESCRIPTION("Test Driver for VGA controller IP.");
 MODULE_LICENSE("Dual BSD/GPL");
+MODULE_ALIAS("custom:vga_dma controller");
 
+#define DEVICE_NAME "IFS"
+#define DRIVER_NAME "IFS_driver"
+#define BUFF_SIZE 20
+#define MAX_PKT_LEN 640*480*4
+
+
+
+//************************************FUNCTION PROTOTYPES************************//
+
+static int __init ifs_init(void);
+static void __exit ifs_exit(void);
+static int ifs_probe(struct platform_device *pdev);
+static int ifs_remove(struct platform_device *pdev);
+int ifs_open(struct inode *pinode, struct file *pfile);
+int ifs_close(struct inode *pinode, struct file *pfile);
+ssize_t ifs_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
+ssize_t ifs_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset);
+static ssize_t ifs_mmap(struct file *pfile, struct vm_area_struct *vma_s);
+
+
+//************************************GLOBAL VARIABLES**********************//
 struct ifs_info {
 
 	unsigned long mem_start;
@@ -42,14 +64,6 @@ static struct ifs_info *ip = NULL;
 u32 *tx_vir_buffer;
 dma_addr_t tx_phy_buffer;
 
-//****************************FUNCTION PROTOTYPES************************//
-static int ifs_probe(struct platform_device *pdev);
-static int ifs_remove(struct platform_device *pdev);
-int ifs_open(struct inode *pinode, struct file *pfile);
-int ifs_close(struct inode *pinode, struct file *pfile);
-ssize_t ifs_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
-ssize_t ifs_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset);
-static ssize_t ifs_mmap(struct file *pfile, struct vm_area_struct *vma_s);
 
 struct file_operations my_fops =
 {
@@ -68,6 +82,8 @@ static struct of_device_id ifs_of_match[] = {
 	{ /*end of list*/ },
 };
 
+MODULE_DEVICE_TABLE (of, ifs_of_match);
+
 static struct platform_driver ifs_driver = {
 
 	.driver ={
@@ -80,33 +96,37 @@ static struct platform_driver ifs_driver = {
 
 };
 
-MODULE_DEVICE_TABLE (of, ifs_of_match);
+//***********************PROBE AND REMOVE************************//
 
 static int ifs_probe (struct platform_device *pdev){
+
 	struct resource *r_mem;
 	int rc = 0;
+	printk(KERN_INFO "vga_dma_probe: Probing\n");
+	//get physical register address space from device tree
 	r_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if(!r_mem ){
-		printk(KERN_ALERT "Failed to get resource\n");
+		printk(KERN_ALERT "IFS_probe: Failed to get resource\n");
 		return -ENODEV;
 	}
-
+	// Get memory for structure ifs_info
 	ip= (struct ifs_info *) kmalloc (sizeof(struct ifs_info),GFP_KERNEL);
 	if (!ip) {
 		printk(KERN_ALERT "could not allocate ifs device\n");
 		return -ENOMEM;
 	}
-
+	// Put phisical adresses in ifs_info structure
 	ip -> mem_start = r_mem -> start;
 	ip -> mem_end = r_mem -> end;
 	//printk(KERN_INFO "Start address: %x \t end address: %x ", r_mem -> start, r_mem -> end)
 
+	// Reserve that memory space for this driver
 	if (!request_mem_region(ip->mem_start,ip->mem_end - ip->mem_start +1 ,DRIVER_NAME )){
 		printk (KERN_ALERT "could not lock memory region at %p \n ", (void *)ip->mem_start);
 		rc = -EBUSY;
 		goto error1;
 	}
-
+	// Remap physical to virtual adresses
 	ip -> base_addr =ioremap (ip-> mem_start, ip->mem_end - ip->mem_start +1);
 	if(!ip->base_addr){
 		printk(KERN_ALERT "could not allocate memory \n");
@@ -114,7 +134,7 @@ static int ifs_probe (struct platform_device *pdev){
 		goto error2;
 	}
 
-	printk(KERN_WARNING "ifs platform driver registered \n");
+	printk(KERN_NOTICE "IFS platform driver registered \n");
 	return 0;
 
 	error2:
@@ -127,7 +147,7 @@ static int ifs_probe (struct platform_device *pdev){
 
 static int ifs_remove(struct platform_device *pdev){
 
-	printk(KERN_WARNING "ifs platform driver removed \n");
+	printk(KERN_WARNING "IFS platform driver removed \n");
 	iowrite32(0, ip->base_addr);
 	iounmap(ip->base_addr);
 	release_mem_region(ip->mem_start,ip->mem_end - ip->mem_start+1);
@@ -150,12 +170,15 @@ int ifs_close(struct inode *pinode, struct file *pfile)
 
 ssize_t ifs_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
 {
+
+		//treba napisati ovde funkcije
 		printk(KERN_INFO "Succesfully read from file\n");
 		return 0;
 }
 
 ssize_t ifs_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
 {
+		//ovde isto treba napraviti
 		printk(KERN_INFO "Succesfully wrote into file\n");
 		return length;
 }
@@ -165,11 +188,14 @@ static ssize_t ifs_mmap(struct file *pfile,struct vm_area_struct *vma_s){
 	int ret =0;
 	long length =vma_s->vm_end -vma_s->vm_start;
 
+	printk(KERN_INFO "DMA TX Buffer is being memory mapped\n");
+
 	if (length > MAX_PKT_LEN)
 	{
 		return -EIO;
 		printk(KERN_ERR "Trying to mmap more space than allowed\n");
 	}
+
 	ret =dma_mmap_coherent(NULL, vma_s, tx_vir_buffer, tx_phy_buffer, length);
 	if(ret<0){
 		printk(KERN_ERR "memory map failed\n");
@@ -181,28 +207,28 @@ static ssize_t ifs_mmap(struct file *pfile,struct vm_area_struct *vma_s){
 
 static int __init ifs_init(void)
 {
-   int i=0;
-   int ret = 0;
-   ret = alloc_chrdev_region(&my_dev_id, 0, 1, DRIVER_NAME);
-   if (ret){
-      printk(KERN_ERR "failed to register char device\n");
-      return ret;
-   }
-   printk(KERN_INFO "char device region allocated\n");
+	int i=0;
+	int ret = 0;
+	ret = alloc_chrdev_region(&my_dev_id, 0, 1, DRIVER_NAME);
+	if (ret){
+		printk(KERN_ERR "failed to register char device\n");
+		return ret;
+	}
+	printk(KERN_INFO "char device region allocated\n");
 
-   my_class = class_create(THIS_MODULE, "ifs_class");
-   if (my_class == NULL){
-      printk(KERN_ERR "failed to create class\n");
-      goto fail_0;
-   }
-   printk(KERN_INFO "class created\n");
-   
-   my_device = device_create(my_class, NULL, my_dev_id, NULL, "ifs");
-   if (my_device == NULL){
-      printk(KERN_ERR "failed to create device\n");
-      goto fail_1;
-   }
-   printk(KERN_INFO "device created\n");
+	my_class = class_create(THIS_MODULE, "ifs_class");
+	if (my_class == NULL){
+		printk(KERN_ERR "failed to create class\n");
+		goto fail_0;
+	}
+	printk(KERN_INFO "class created\n");
+
+	my_device = device_create(my_class, NULL, my_dev_id, NULL, "ifs");
+	if (my_device == NULL){
+		printk(KERN_ERR "failed to create device\n");
+		goto fail_1;
+	}
+	printk(KERN_INFO "device created\n");
 
 	my_cdev = cdev_alloc();	
 	my_cdev->ops = &my_fops;
@@ -210,11 +236,11 @@ static int __init ifs_init(void)
 	ret = cdev_add(my_cdev, my_dev_id, 1);
 	if (ret)
 	{
-      printk(KERN_ERR "failed to add cdev\n");
+		printk(KERN_ERR "failed to add cdev\n");
 		goto fail_2;
 	}
-   printk(KERN_INFO "cdev added\n");
-   printk(KERN_INFO "ifs: Module init done\n");
+	printk(KERN_INFO "cdev added\n");
+	printk(KERN_INFO "ifs: Module init done\n");
 
 	tx_vir_buffer = dma_alloc_coherent (NULL,MAX_PKT_LEN, &tx_phy_buffer, GFP_DMA | GFP_KERNEL);
 	if(!tx_vir_buffer){
@@ -227,11 +253,11 @@ static int __init ifs_init(void)
 
 	for( i = 0; i<MAX_PKT_LEN/4;i++)
 		tx_vir_buffer[i]=0x00000000;
-	printk(KERN_INFO"ifs_init: Memory reset");
+		printk(KERN_INFO"ifs_init: Memory reset");
 
 
-   return 0;
-	
+	return 0;
+
 	fail_3:
 		cdev_del(my_cdev);
 	fail_2:
@@ -239,19 +265,19 @@ static int __init ifs_init(void)
 	fail_1:
 		class_destroy(my_class);
 	fail_0:
-      unregister_chrdev_region(my_dev_id, 1);
-   return -1;
+	unregister_chrdev_region(my_dev_id, 1);
+	return -1;
 }
 
 static void __exit ifs_exit(void)
 {
 	platform_driver_unregister(&ifs_driver);
 	cdev_del(my_cdev);
-   device_destroy(my_class, my_dev_id);
-   class_destroy(my_class);
-   unregister_chrdev_region(my_dev_id,1);
-   dma_free_coherent(NULL, MAX_PKT_LEN, tx_vir_buffer, tx_phy_buffer);
-   printk(KERN_INFO "Goodbye, cruel world\n");
+	device_destroy(my_class, my_dev_id);
+	class_destroy(my_class);
+	unregister_chrdev_region(my_dev_id,1);
+	dma_free_coherent(NULL, MAX_PKT_LEN, tx_vir_buffer, tx_phy_buffer);
+	printk(KERN_INFO "Goodbye, cruel world\n");
 }
 
 
