ecd86b11d4b0b4eeb723b91607aeee35f80f7931
Milan Simin <simin.milan34@gmail.com>
2020-06-08 21:04:34 +0200

    new code

--- VP/test/convert.py
+++ VP/test/convert.py
@@ -3,28 +3,28 @@ import numpy
 import sys
 
 img = cv2.imread(str(sys.argv[1]))
-im = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+width = 256;
+height = 256;
+dim = (width, height)
+# resize image
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+im = cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
 
 with open("image.txt", "w") as text_file:
-    cut_size_x = 512
-    cut_size_y = 512
-    x_size = 0
-    for x in im:
-        y_size = 0
-        x_size += 1
-        if (x_size>cut_size_x) :
-            break
-        for y in x:
-            y_size += 1
-            if (y_size>cut_size_y) :
-                break
-            count = 0
-            for z in y:
-                count+=1
-                print(z, file=text_file, end='')
-                if count != 3:
-                    print(' ', file=text_file, end='')
-            print(' ', file=text_file, end='')
-        print('\n', file=text_file, end='')
+	cut_size_x = 256
+	cut_size_y = 256
+	x_size = 0
+	for x in im:
+		y_size = 0
+		x_size += 1
+		if (x_size>cut_size_x) :
+			break
+		for y in x:
+			y_size += 1
+			print(y, file=text_file, end=' ')
+			if (y_size>cut_size_y) :
+				break                     
+		print('\n', file=text_file, end='')
 
 
--- VP/test/read.py
+++ VP/test/read.py
@@ -6,24 +6,32 @@ from PIL import Image
 
 
 with open("final_image.txt", "r") as text_file:
-    iksevi = text_file.readlines()
-    kolone = iksevi[1].split(' ')
 
-    imgArray = numpy.empty([len(iksevi),math.ceil((len(kolone))/3), 3], dtype=numpy.int8)
-    xerino = 0
-    for x in iksevi:
-        brojevi = x.split(" ")
-        counter = 0
-        y = -1
-        for broj in brojevi:
-            if (counter % 3 == 0):
-                y+=1
-                counter = 0
-            if (broj != '\n'):
-             imgArray[xerino][y][counter] = int(broj)
-            counter+=1
-        xerino+=1
+	iksevi = text_file.readlines() #iksevi = redovi
+	kolone = iksevi[1].split(' ')
+	#print('iksevi : ',len(iksevi))
+	#print('kolone : ',len(kolone))
+	imgArray = numpy.empty([len(iksevi),len(kolone)], dtype=numpy.int8)
+	xerino = 0
 
-    img = Image.fromarray(imgArray,'RGB')
-    img.show()
+	for x in iksevi:
+		brojevi = x.split(" ")
+		#counter = 0
+		y = 0
+		for broj in brojevi:
+			#if (counter % 3 == 0):
+			#counter = 0
+			if (broj != '\n'):						
+				imgArray[xerino][y] = int(broj)
+				y+=1
+			#counter+=1
+		xerino+=1
+
+	img = Image.fromarray(imgArray)
+	img.show()
+
+
+
+
+
 
