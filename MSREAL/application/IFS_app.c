73fcad711fe5a0dad7d2115b7f56e0b61f61c4a1
Milan Simin <simin.milan34@gmail.com>
2020-06-12 00:10:06 +0000

    working

--- MSREAL/application/IFS_app.c
+++ MSREAL/application/IFS_app.c
@@ -7,17 +7,19 @@
 
 #include "image.h"
 
-#define MAX_PKT_SIZE 57600
+#define MAX_BRAM_SIZE 57600
 #define MAX_IFS_SIZE 16384
+#define MAX_KERNEL_SIZE 4096
+#define IFS_SEND 16
+#define KERNEL_SEND 36
 #define MMAP
 
-int kernel[9]={-1,-1,-1,-1,8,-1,-1,-1,-1};
-int ifs[4]={121,120,1,0};
-int *final_image;
 
-
-int main(void)
-{
+int main(void){
+	int kernel[9]={-1,-1,-1,-1,8,-1,-1,-1,-1};
+	int lines=120,columns=121,start=1,num_of_bytes=10;
+	int ifs_reg[4]={columns,lines,start,0};
+	int *final_image, *ready;
 	FILE *fm;
 	int fp,fr,fk,fn;
 	int *p,*r,*k,*n;
@@ -29,14 +31,18 @@ int main(void)
 		printf("Cannot open /dev/image_conv for write\n");
 		return -1;
 	}
+        ready = (int *) malloc(num_of_bytes);
 	p=(int*)mmap(0, MAX_IFS_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fp, 0);
 	if (p == NULL ) {
 		printf ("\ncouldn't mmap\n");
 		return 0;
 	}
-	memcpy(p, ifs, 16);
-	munmap(p, 16);
+
+	memcpy(p, ifs_reg, IFS_SEND);
+	//memcpy(ready, p, IFS_SEND/4);
+	munmap(p, IFS_SEND);
 	printf("image_conv done\n");
+	//printf("ready is %d\n",&ready);
 	close(fp);
 	if (fp < 0)
 	{
@@ -51,16 +57,13 @@ int main(void)
 		printf("Cannot open /dev/bram_image for write\n");
 		return -1;
 	}
-	r=(int*)mmap(0,MAX_PKT_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fr, 0);
+	r=(int*)mmap(0,MAX_BRAM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fr, 0);
 	if (r == NULL ) {
 		printf ("\ncouldn't mmap\n");
 		return 0;
 	}
-	//printf("provera 1\n");
-	memcpy(r, image,MAX_PKT_SIZE);
-	//printf("provera 2\n");
-	munmap(r, MAX_PKT_SIZE);
-	//printf("provera 3\n");
+	memcpy(r, image,MAX_BRAM_SIZE);
+	munmap(r, MAX_BRAM_SIZE);
 	printf("bram_image done\n");
 	close(fr);
 	if(fr < 0)
@@ -75,14 +78,14 @@ int main(void)
 		printf("Cannot open /dev/bram_kernel for write\n");
 		return -1;
 	}
-	k=(int*)mmap(0,4096, PROT_READ | PROT_WRITE, MAP_SHARED, fk, 0);
+	k=(int*)mmap(0,MAX_KERNEL_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fk, 0);
 	if (k == NULL ) {
 		printf ("\ncouldn't mmap\n");
 		return 0;
 	}
 
-	memcpy(k, kernel, 36);
-	munmap(k, 36);
+	memcpy(k, kernel, KERNEL_SEND);
+	munmap(k, KERNEL_SEND);
 	printf("bram_kernel done\n");
 	close(fk);
 	if(fk < 0)
@@ -98,15 +101,15 @@ int main(void)
 		printf("Cannot open /dev/bram_after_conv for write\n");
 		return -1;
 	}
-	final_image = (int *) malloc(MAX_PKT_SIZE);
-	n=(int*)mmap(0,MAX_PKT_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fn, 0);
+	final_image = (int *) malloc(MAX_BRAM_SIZE);
+	n=(int*)mmap(0,MAX_BRAM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fn, 0);
 	if (n == NULL ) {
 		printf ("\ncouldn't mmap\n");
 		return 0;
 	}
 	
-	memcpy(final_image, n, MAX_PKT_SIZE);
-	munmap(n, MAX_PKT_SIZE);
+	memcpy(final_image, n, MAX_BRAM_SIZE);
+	munmap(n, MAX_BRAM_SIZE);
 	printf("bram_after_conv done\n");
 	close(fn);
 	if(fn < 0)
