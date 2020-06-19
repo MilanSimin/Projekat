1cfe06c74466982f35ede416a8a690304f4bf43a
MilanSimin <57679674+MilanSimin@users.noreply.github.com>
2020-06-19 18:30:26 +0200

    Create axi_lite_common.sv

---
+++ verif/uvc_lib/axi_lite/sv/axi_lite_common.sv
@@ -0,0 +1,11 @@
+`ifndef AXI_LITE_COMMON_SV
+`define AXI_LITE_COMMON_SV
+
+parameter int MAX_VALUE = 255;
+parameter int WIDTH = 9;
+parameter int COLUMNS_REG_ADDR = 0;
+parameter int LINES_REG_ADDR = 4;
+parameter int START_REG_ADDR = 8;
+parameter int READY_REG_ADDR = 12;
+
+`endif // AXI_LITE_COMMON_SV
